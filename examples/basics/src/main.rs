50a9a8e2466841859fb57297e7d92e6fab1fad88
Dave Bryson <davebryson@users.noreply.github.com>
2023-07-30 14:29:49 -0500

    clean-up example

--- examples/basics/src/main.rs
+++ examples/basics/src/main.rs
@@ -3,17 +3,18 @@ mod erc20;
 use erc20::MockErc20;
 use revmup_client::RevmClient;
 
-fn run_erc(provider: ::std::sync::Arc<revmup_client::BasicClient>) {
+fn main() {
+    let client = ::std::sync::Arc::new(revmup_client::BasicClient::new());
     let amt = ::ethers::core::utils::parse_ether(3u8).unwrap();
 
-    let accounts = provider
+    let accounts = client
         .batch_create_accounts_with_balance(2, amt)
         .expect("acccounts");
     let alice = accounts[0];
     let bob = accounts[1];
 
     let addy = MockErc20::deploy::<(String, String, u8)>(
-        provider.clone(),
+        client.clone(),
         bob,
         ("hello".into(), "H".into(), 8u8),
     )
@@ -21,29 +22,33 @@ fn run_erc(provider: ::std::sync::Arc<revmup_client::BasicClient>) {
     println!("address: {}", addy);
 
     // instance
-    let erc = MockErc20::new(addy, provider.clone());
+    let erc = MockErc20::new(addy, client.clone());
 
-    // methods
+    // call methods
     let v = erc.name().call().unwrap();
     println!("name: {}", v);
 
+    // send tx
     erc.mint(bob, 2u8.into()).send_transaction(bob).unwrap();
+    // note caller (from) is bob ---------------^
 
     let b = erc.balance_of(bob).call().unwrap();
     println!("bal: {:?}", b);
 
+    // view logs
     let (_, logs) = erc
         .transfer(alice.into(), 1u8.into())
         .send_transaction(bob)
         .unwrap();
 
-    let lr = erc
+    let tlogs = erc
         .get_transfer_filter_logs(logs.clone())
         .expect("parse log");
-    println!("EVENT: {:?}", lr);
+    println!("events: {:?}", tlogs);
 
-    let lr = erc.get_approval_filter_logs(logs).expect("parse log");
-    println!("EVENT: {:?}", lr);
+    // This will be '[]'
+    let alogs = erc.get_approval_filter_logs(logs).expect("parse log");
+    println!("events: {:?}", alogs);
 
     let u = erc.balance_of(bob).call().unwrap();
     println!("user bal: {:?}", u);
@@ -51,11 +56,5 @@ fn run_erc(provider: ::std::sync::Arc<revmup_client::BasicClient>) {
     let alicebal = erc.balance_of(alice.into()).call().unwrap();
     println!("alice bal: {:?}", alicebal);
 
-    println!("provider bal for alice: {:}", provider.get_balance(alice))
-}
-
-fn main() {
-    let provider = ::std::sync::Arc::new(revmup_client::BasicClient::new());
-
-    run_erc(provider);
+    println!("client bal for alice: {:}", client.get_balance(alice))
 }
